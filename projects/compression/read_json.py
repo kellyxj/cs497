3ed85746b5c6e9c5caf8f363f08753bba1ba45d8
Kelly <kllyjiang@gmail.com>
2023-10-09 11:03:31 -0500

    add sqlite files

---
+++ projects/compression/read_json.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pyspark
+from pyspark.sql import SparkSession
+import os
+import sqlite3
+
+filename = "questions"
+json_path = os.path.join(os.getcwd(), "data", filename + ".json")
+
+spark = SparkSession.builder.getOrCreate()
+df = spark.read.json(json_path)
+
+print(df.printSchema())
+df_pd = df.toPandas()
+
+conn = sqlite3.connect(os.path.join(os.getcwd(), "data", filename + ".db"))
+df_pd.to_sql(filename, conn, index=True, if_exists="replace")
