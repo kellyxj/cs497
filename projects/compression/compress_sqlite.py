e97e84ef562bc5b3b471e88fca7c5eca1bcc6ef2
Kelly <kllyjiang@gmail.com>
2023-10-24 10:19:48 -0500

    add method to cast column to int

--- projects/compression/compress_sqlite.py
+++ projects/compression/compress_sqlite.py
@@ -1,5 +1,6 @@
 from compression_schemes.dictionary_encode import DictionaryEncode
 from compression_schemes.bitpack import Bitpack
+from util.get_size import get_obj_size 
 
 import os
 import sys
@@ -7,48 +8,86 @@ import sys
 import sqlite3
 from decimal import *
 
-def try_cast_to_int(column):
-    pass
+class Runner():
+    def __init__(self, filename):
+        self.filename = filename
+        self.read_con = sqlite3.connect(os.path.join(os.getcwd(), "data", filename + ".db"))
+        self.read_cursor = self.read_con.cursor()
+        self.write_con = sqlite3.connect(os.path.join(os.getcwd(), "data", filename + "_compressed.db"))
+        self.write_cursor = self.write_con.cursor()
 
-def try_cast_to_decimal(column):
-    pass
+    def try_cast_to_int(self, column):
+        new_column = []
+        try:
+            for row in column:
+                new_row = (int(row[0]))
+                new_column.append(new_row)
+            print("cast succeeded")
+            return new_column
+        except(TypeError, ValueError):
+            print("cast failed")
+            return column
 
-def try_dictionary_encode(column):
-    dictionary_encode = DictionaryEncode()
-    dictionary_encode.compress(column)
+    def try_cast_to_decimal(self, column):
+        pass
 
-def try_bitpack(column):
-    pass
+    def try_dictionary_encode(self, column):
+        dictionary_encode = DictionaryEncode()
+        new_column = dictionary_encode.compress(column)
 
-def compress(filename):
-    filename = "clothes"
-    con = sqlite3.connect(os.path.join(os.getcwd(), "data", filename + ".db"))
+        print("Original size: " + str(get_obj_size(column)))
+        print("Dictionary size: " + str(get_obj_size(dictionary_encode.dictionary)))
+        print("New column size: " + str(get_obj_size(new_column)))
 
-    
-    bitpack = Bitpack()
+    def try_bitpack(self, column):
+        bitpack = Bitpack()
+        new_column = bitpack.compress(column)
 
-    cur = con.cursor()
-    column_name_query = "SELECT name FROM PRAGMA_TABLE_INFO('" + filename + "');"
-    res = cur.execute(column_name_query)
+        print("Original size: " + str(get_obj_size(column)))
+        print("Bitpacking metadata size: ")
+        print("New column size: " + str(get_obj_size(new_column)))
 
-    column_names = res.fetchall()
-    for column_name in column_names:
-        query = "SELECT \"" + column_name[0] + "\" FROM " + filename + ";"
-        print(query) 
+    def compress(self):
+ 
+        column_name_query = "SELECT name FROM PRAGMA_TABLE_INFO('" + self.filename + "');"
+        res = self.read_cursor.execute(column_name_query)
 
-        if column_name[0] != "index":
-            type_query = "SELECT type FROM PRAGMA_TABLE_INFO('" + filename + "') WHERE name = \"" + column_name[0] + "\";"
-            res = cur.execute(type_query)
-            column_type =  res.fetchall()[0][0]
 
-            res = cur.execute(query)
-            column = res.fetchall()
+        column_names = res.fetchall()
+        for column_name in column_names:
+            #start by creating a metadata table
+            #for each column in the original table, store a bit vector
+            #set the bit to 1 if compression scheme is applied
 
-            if column_type == "TEXT":
-               try_dictionary_encode(column)
-    print("done")
 
-def decompress(filename):
-    pass
+            #for each column, try casting to int
+            #if that fails, try casting to float
 
-compress("clothes")
+            #for each column, try each compression scheme and see if the result is smaller
+
+            query = "SELECT \"" + column_name[0] + "\" FROM " + self.filename + ";"
+            print(query) 
+
+            if column_name[0] != "index":
+                type_query = "SELECT type FROM PRAGMA_TABLE_INFO('" + self.filename + "') WHERE name = \"" + column_name[0] + "\";"
+                res = self.read_cursor.execute(type_query)
+                column_type =  res.fetchall()[0][0]
+
+                res = self.read_cursor.execute(query)
+                column = res.fetchall()
+
+                if column_type == "TEXT":
+                   self.try_cast_to_int(column)
+        print("done")
+
+    def decompress(self):
+        #for each column, check the bit in the metadata table to see if compression scheme was applied
+        #if it was applied, read from the correct auxiliary table to decompress
+        pass
+
+    def create_metadata(self):
+
+        pass
+
+runner = Runner("clothes")
+runner.compress()
--- projects/compression/compression_schemes/dictionary_encode.py
+++ projects/compression/compression_schemes/dictionary_encode.py
@@ -1,21 +1,17 @@
-from util.get_size import get_obj_size
-
 class DictionaryEncode():
     def __init__(self):
-        pass
+        self.dictionary = {}
 
     def compress(self, column):
-        dictionary = {}
         new_column = []
         count = 0
         for row in column:
-            if not row[0] in dictionary:
-                dictionary[row[0]] = count
+            if not row[0] in self.dictionary:
+                self.dictionary[row[0]] = count
                 count += 1
-            new_column.append(dictionary[row[0]])
-        print("Original size: " + str(get_obj_size(column)))
-        print("Dictionary size: " + str(get_obj_size(dictionary)))
-        print("New column size: " + str(get_obj_size(new_column)))
+            new_column.append(self.dictionary[row[0]])
+        
+        return new_column
 
     def decompress(self, column):
         pass
