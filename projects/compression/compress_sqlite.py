67bbfed8ffd6ccc77da64ea45f6c219664f7ad87
Kelly <kllyjiang@gmail.com>
2023-10-25 08:44:31 -0500

    implement dictionary encoding

--- projects/compression/compress_sqlite.py
+++ projects/compression/compress_sqlite.py
@@ -15,7 +15,12 @@ class Runner():
         self.read_cursor = self.read_con.cursor()
         self.write_con = sqlite3.connect(os.path.join(os.getcwd(), "data", filename + "_compressed.db"))
         self.write_cursor = self.write_con.cursor()
+
+        self.dictionary_encode = DictionaryEncode()
+        self.bitpack = Bitpack()
+
         self.cast_succeeded = False
+        self.dictionary_encoded = False
 
     def try_cast_to_int(self, column):
         new_column = []
@@ -33,16 +38,25 @@ class Runner():
         pass
 
     def try_dictionary_encode(self, column):
-        dictionary_encode = DictionaryEncode()
-        new_column = dictionary_encode.compress(column)
+        new_column = self.dictionary_encode.compress(column)
 
-        print("Original size: " + str(get_obj_size(column)))
-        print("Dictionary size: " + str(get_obj_size(dictionary_encode.dictionary)))
-        print("New column size: " + str(get_obj_size(new_column)))
+        original_size = get_obj_size(column)
+        dictionary_size = get_obj_size(self.dictionary_encode.dictionary) + get_obj_size(self.dictionary_encode.dictionary.keys())
+        new_column_size = get_obj_size(new_column)
+
+        print("Original size: " + str(original_size))
+        print("Dictionary size: " + str(dictionary_size))
+        print("New column size: " + str(new_column_size))
+
+        if new_column_size + dictionary_size < original_size:
+            self.dictionary_encoded = True
+            return new_column
+        else:
+            self.dictionary_encoded = False
+            return column
 
     def try_bitpack(self, column):
-        bitpack = Bitpack()
-        new_column = bitpack.compress(column)
+        new_column = self.bitpack.compress(column)
 
         print("Original size: " + str(get_obj_size(column)))
         print("Bitpacking metadata size: ")
@@ -68,7 +82,7 @@ class Runner():
             query = "SELECT \"" + column_name[0] + "\" FROM " + self.filename + ";"
             print(query) 
 
-            if column_name[0] != "index":
+            if column_name[0] != "index" and column_name[0] != "_corrupt_record":
                 type_query = "SELECT type FROM PRAGMA_TABLE_INFO('" + self.filename + "') WHERE name = \"" + column_name[0] + "\";"
                 res = self.read_cursor.execute(type_query)
                 column_type =  res.fetchall()[0][0]
@@ -80,15 +94,39 @@ class Runner():
                    column = self.try_cast_to_int(column)
 
                 column_type = "INTEGER" if self.cast_succeeded  else column_type
-                create_table_statement = "CREATE TABLE IF NOT EXISTS " + column_name[0].replace(" ", "_") + "(\"" + column_name[0] + "\" " + column_type + ");"
+                table_name = column_name[0].replace(" ", "_")
+                create_table_statement = "CREATE TABLE IF NOT EXISTS " + table_name + "(\"" + column_name[0] + "\" " + column_type + ");"
                 self.write_cursor.execute(create_table_statement)
-                #self.write_con.commit()
-                #print(create_table_statement)
-                insert_statement = "INSERT INTO " + column_name[0].replace(" ", "_") + "(\"" + column_name[0] + "\")" + " VALUES (?);"
-                #print(insert_statement)
+
+                insert_statement = "INSERT INTO " + table_name + "(\"" + column_name[0] + "\")" + " VALUES (?);"
 
                 self.write_cursor.executemany(insert_statement, column)
-            self.write_con.commit()
+                self.write_con.commit()
+
+                drop_table_statement = "DROP TABLE " + table_name
+
+                if column_type == "TEXT":
+                    column = self.try_dictionary_encode(column)
+                    if self.dictionary_encoded:
+                        create_table_statement = "CREATE TABLE IF NOT EXISTS " + table_name + "_dictionary_encoded" + "(\"key\" INTEGER);"
+                        self.write_cursor.execute(create_table_statement)
+
+                        insert_statement = "INSERT INTO " + table_name + "_dictionary_encoded" + "(\"key\")" + " VALUES (?);"
+                        self.write_cursor.executemany(insert_statement, column)
+
+                        create_table_statement = "CREATE TABLE IF NOT EXISTS " + table_name + "_dictionary" + "(\"key\" INTEGER, \"" + column_name[0] + "\");"
+                        self.write_cursor.execute(create_table_statement)
+
+                        dictionary = self.dictionary_encode.dictionary
+                        for value in dictionary.keys():
+                            value_string = value.replace("'","''") if value != None else "NULL"
+                            insert_statement = "INSERT INTO " + table_name + "_dictionary" + "(\"key\", \"" + column_name[0] + "\") VALUES (" + str(dictionary[value])  + ", '" + value_string + "');"
+                            self.write_cursor.execute(insert_statement)
+
+                        self.write_cursor.execute(drop_table_statement)
+                        self.write_con.commit()
+
+                self.dictionary_encode.dictionary = {}
         print("done")
 
     def decompress(self):
@@ -101,4 +139,10 @@ class Runner():
         pass
 
 runner = Runner("clothes")
+runner.compress()
+
+runner = Runner("news")
+runner.compress()
+
+runner = Runner("questions")
 runner.compress()
